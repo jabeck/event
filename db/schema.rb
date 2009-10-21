9ab84ba60cd436874ce4c6496b569682ad5e9767
jabeck <jabeck@nmu.edu>
2009-10-20 21:21:31 -0400

    Started working on the look and layout. 'New' page is mostly done.

--- db/schema.rb
+++ db/schema.rb
@@ -29,14 +29,12 @@ ActiveRecord::Schema.define(:version => 20091006223622) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.integer  "facebook_uid",       :limit => 8
-    t.string   "email",                                          :null => false
-    t.string   "crypted_password",                               :null => false
-    t.string   "password_salt",                                  :null => false
-    t.string   "persistence_token",                              :null => false
-    t.integer  "login_count",                     :default => 0, :null => false
-    t.integer  "failed_login_count",              :default => 0, :null => false
+    t.string   "email",                             :null => false
+    t.string   "crypted_password",                  :null => false
+    t.string   "password_salt",                     :null => false
+    t.string   "persistence_token",                 :null => false
+    t.integer  "login_count",        :default => 0, :null => false
+    t.integer  "failed_login_count", :default => 0, :null => false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
---
+++ public/javascripts/jToday.js
@@ -0,0 +1,54 @@
+﻿// jToday a simple jQuery date display plugin by Donavon West
+// Copyright 2009 Donavon West. All rights reserved
+
+(function($, document) {
+//--------------------
+$.fn.jToday = function(p_date) {
+
+	var jTodayData = "jTodayData", div = "div", match, data, months = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ");
+
+	// ------------------------
+	// set the class and inner HTML
+	function setStuff(p_el, p_class, p_str) {
+		p_el.innerHTML = p_str;
+		p_el.className = p_class + " " + p_class + p_str;
+	}
+	
+	// ------------------------
+	//constructor code
+
+	return this.each(function() {
+		data = $.data(this, jTodayData);
+		if (!data) { //only done once per element
+			data = {};
+			
+			//does innerHTML contain a date string? (i.e. YYYY-MM-DD)
+			match = this.innerHTML.match(/^(\d\d\d\d)-(\d\d)-(\d\d)( |$|T)/);
+			if (match) {
+				data.date = new Date(match[1], parseInt(match[2],10)-1, match[3]); //yes, use it
+			} else {
+				data.date = p_date ? p_date : new Date(); //use the date passed in (or default to today)
+			}
+
+			//create the sub elements
+			this.innerHTML = "";
+			data.yearEl = document.createElement(div);
+			this.appendChild(data.yearEl);
+			data.monthEl = document.createElement(div);
+			this.appendChild(data.monthEl);
+			data.dayEl = document.createElement(div);
+			this.appendChild(data.dayEl);
+			$.data(this, jTodayData, data); //save it for next time
+		} else {
+			data.date = p_date ? p_date : data.date; //use the date passed in (or the old value if undefined)
+		}
+
+		setStuff(data.yearEl, "year", data.date.getFullYear().toString() );
+		setStuff(data.monthEl, "month", months[data.date.getMonth()] );
+		setStuff(data.dayEl, "day", (data.date.getDate()).toString() );
+	});
+
+};
+
+//--------------------
+})(jQuery, document); //minify trick, plus don't EVER assume that $ is the jQuery object. it's just bad
